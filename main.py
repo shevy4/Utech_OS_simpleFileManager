e4d9a58e2d2475808c8e48dbb9b6664962ee251b
shav <61606455+shevy4@users.noreply.github.com>
2023-11-01 14:45:28 -0500

    Update main.py
    read and size function implementation

--- main.py
+++ main.py
@@ -10,7 +10,70 @@ file_b_content = "This program has performed an illegal operation and will be sh
 file_c_content = "From tiny ACORNS mighty UNIX trees grow."
 
 
+def file_innit():
+    block = ""
+    for x, y in enumerate(file_a_content, 1):
+        block += y
+        if x % 8 == 0 and x != 0:
+            FILESYSTEM.append([block])
+            block = ""
+        if x == len(file_a_content):
+            FILESYSTEM.append([block])
+            block = ""
+
+    DIRECTORY.append(["A", "1," + str(len(FILESYSTEM))])
+
+    for x, y in enumerate(file_b_content, 1):
+        block += y
+        if x % 8 == 0 and x != 0:
+            FILESYSTEM.append([block])
+            block = ""
+        if x == len(file_b_content):
+            FILESYSTEM.append([block])
+            block = ""
+
+    DIRECTORY.append(["B", str(int(str(DIRECTORY[0][1]).split(",")[1]) + 1) + "," + str(len(FILESYSTEM))])
+
+    for x, y in enumerate(file_c_content, 1):
+        block += y
+        if x % 8 == 0 and x != 0:
+            FILESYSTEM.append([block])
+            block = ""
+        if x == len(file_c_content) and not y:
+            FILESYSTEM.append([block])
+            block = ""
+
+    DIRECTORY.append(["C", str(int(str(DIRECTORY[1][1]).split(",")[1]) + 1) + "," + str(len(FILESYSTEM))])
+
+
+def get_size(file):
+    characters = 0
+    for x, y in enumerate(DIRECTORY):
+        if file == str(y[0]).casefold():
+            blocks = int(str(y[1]).split(",")[1]) - (int(str(y[1]).split(",")[0])) + 1
+            for z in FILESYSTEM[int(str(y[1]).split(",")[0]) - 1:int(str(y[1]).split(",")[1])]:
+                for count2 in str(z):
+                    if count2 != "[" and count2 != "]" and count2 != "'":
+                        characters += 1
+            print("File", file, "Size :", characters, "Characters,", blocks, "Blocks")
+            break
+
+    else:
+        print("File Not In Filesystem")
+    return 0
+
+
+def read_file(file, arg):
+    for x, y in DIRECTORY:
+        if x == file.upper():
+            # print(str(y).split(",")[0], " + ", str(y).split(",")[1])
+            for z in range(int(str(y).split(",")[0]) - 1, int(str(y).split(",")[1])):
+                print(FILESYSTEM[z])
+            break
+
+
 def menu():
+    file_innit()
     while 1:
 
         choice = input().casefold()
@@ -32,13 +95,21 @@ def menu():
                         print("Allows the user to read all contents of a file or a specific character at the front, "
                               "middle, or back")
             case "size":
-                print("SIZE")
+                try:
+                    get_size(choice.split(" ")[1])
+                except IndexError:
+                    print("Total system blocks = ", len(FILESYSTEM))
 
             case "read":
                 try:
-                    print(choice.split(" ")[1])
+                    read_file(choice.split(" ")[1], choice.split(" ")[2])
+
                 except IndexError:
-                    print(choice.split(" ")[0])
+                    try:
+                        read_file(choice.split(" ")[1], " ")
+
+                    except IndexError:
+                        print(choice.split(" ")[0])
 
             case "del":
                 try:
